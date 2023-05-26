10e6f8cea270daa0145134cae8831edb1849e75f
Biteau Gaël <61157802+Biteau-Gael@users.noreply.github.com>
2023-05-26 16:22:37 +0200

    ECF STUDI GARAGE

---
+++ src/Entity/VoitureOccasion.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use AllowDynamicProperties;
+use App\Repository\VoitureOccasionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VoitureOccasionRepository::class)
+ */
+#[AllowDynamicProperties] class VoitureOccasion
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $marque;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $modele;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $annee;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=0)
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Kilometre;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): self
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): self
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getAnnee(): ?int
+    {
+        return $this->annee;
+    }
+
+    public function setAnnee(int $annee): self
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getKilometre(): ?int
+    {
+        return $this->Kilometre;
+    }
+
+    public function setKilometre(int $Kilometre): self
+    {
+        $this->Kilometre = $Kilometre;
+
+        return $this;
+    }
+}
